67cd5c43f85b15e9b4dca72825791f191f51f491
Hawk94 <x99tom.miller@gmail.com>
2017-08-27 14:07:59 +0100

    Adding about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import whiteCurve from '../../Assets/images/white-curve.svg'
 
 import "./about.css"
 
@@ -6,30 +7,30 @@ import "./about.css"
 export default class About extends Component {
   render () {
     return (
-      <section className='About'>
-        <div class="curve"><img src="img/white-curve.svg" alt="" /></div>
-        <div class="container">
-          <h2 class="section-header">How does it work?</h2>
-          <div class="divider light"></div>
+      <section className='about'>
+        <div className="white-curve"><img src={whiteCurve} alt="" /></div>
+        <div className="container">
+          <h2 className="section-header">How does it work?</h2>
+          <div className="divider light"></div>
 
-          <div class="carousel-holder wow fadeIn">
-            <span class="carousel-arw prev"></span>
-            <span class="carousel-arw next"></span>
-            <div class="carousel">
+          <div className="carousel-holder wow fadeIn">
+            <span className="carousel-arw prev"></span>
+            <span className="carousel-arw next"></span>
+            <div className="carousel">
               <img src="img/step-1.png" alt="" />
               <img src="img/step-2.png" alt="" />
               <img src="img/step-3.png" alt="" />
             </div>
           </div>
 
-          <p class="carousel-tip">Make a purchase in-store or online using your linked card</p>
+          <p className="carousel-tip">Make a purchase in-store or online using your linked card</p>
 
-          <div class="carousel-dots"></div>
+          <div className="carousel-dots"></div>
 
-          <div class="join">
-            <h2 class="section-header  wow fadeIn">Dust is currently in  alpha. If you’d like to get your hands on it early, please request an invite</h2>
-            <div class="divider light"></div>
-            <a href="#" class="btn primary signup wow fadeIn" data-wow-delay=".25s">Join the alpha</a>
+          <div className="join">
+            <h2 className="section-header  wow fadeIn">Dust is currently in  alpha. If you’d like to get your hands on it early, please request an invite</h2>
+            <div className="divider light"></div>
+            <a href="#" className="btn primary signup wow fadeIn" data-wow-delay=".25s">Join the alpha</a>
           </div>
         </div>
       </section>
